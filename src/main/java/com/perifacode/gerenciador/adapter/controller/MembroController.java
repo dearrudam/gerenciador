719747b5ec83cc824bcb62a8fad5f053a697326e
TIOZAO <marco_aurelioo@yahoo.com.br>
2020-11-10 23:14:08 -0300

    validações membro controller para busca por dataInclusao

--- src/main/java/com/perifacode/gerenciador/adapter/controller/MembroController.java
+++ src/main/java/com/perifacode/gerenciador/adapter/controller/MembroController.java
@@ -4,10 +4,18 @@ import com.perifacode.gerenciador.adapter.common.MembroConverter;
 import com.perifacode.gerenciador.adapter.presenters.MembroDto;
 import com.perifacode.gerenciador.entity.Membro;
 import com.perifacode.gerenciador.usecase.MembroService;
+import java.awt.print.Pageable;
+import java.util.List;
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import org.apache.coyote.Response;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +23,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RestController
 @RequestMapping("/membros")
 public class MembroController {
@@ -39,6 +49,31 @@ public class MembroController {
     Membro membro = membroService.buscar(email);
     return membroConverter.convertFromEntity(membro);
   }
+
+
+  @GetMapping(path = "/")
+  public ResponseEntity<String> buscarByDataInclusao(
+      @Valid
+        @RequestParam(name = "dataInclusao.range", required = false)
+          @Size(max = 2, min = 2)
+          List<@Pattern(regexp = "\\d{2}-\\d{2}-\\d{4}") String> dataInclusaoRange,
+      @Valid @Pattern(regexp = "\\d{2}-\\d{2}-\\d{4}")
+        @RequestParam(name = "dataInclusao", required = false) String dataInclusao,
+      @Valid @Pattern(regexp = "\\d{2}-\\d{2}-\\d{4}")
+        @RequestParam(name = "dataInclusao.ge", required = false) String dataInclusaoGe,
+      @RequestParam(defaultValue = "0") int page,
+      @RequestParam(defaultValue = "20") int size) {
+
+
+    if(dataInclusao != null){
+
+    }
+    if(dataInclusaoGe != null){
+        // busca todos maiores que determinada data
+    }
+    return ResponseEntity.ok( "ok");
+  }
+
 
   @PutMapping(value = "/{membro_id}")
   public ResponseEntity<MembroDto> updateMembro(@PathVariable("membro_id") Long membroId,
